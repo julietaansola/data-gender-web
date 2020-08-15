23ed8647b1f5c2bbfcdcb6741ec52abd5d502635
camila-ng <caming2309@gmail.com>
2020-08-15 12:05:18 -0300

    add members

--- src/components/who-we-are/WhoWeAre.js
+++ src/components/who-we-are/WhoWeAre.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import info from "../../data.js";
+import './who-we-are.scss'
 
 const WhoWeAre = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="who-we-are-content">
+      <h1 className="who-we-are-title">Quienes somos</h1>
+      <section className="member-container">
+        {info.map((member) => (
+          <div key={member.name} className="member">
+            <img alt={`miembro ${member.name}`} src={member.image} className="member-image"></img>
+            <div className="member-name">{member.name}</div>
+            <div className="member-description">{member.description}</div>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
+};
 
-export default WhoWeAre
+export default WhoWeAre;
