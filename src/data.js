927af25f77357b3e9b21e4c8e23c12a09f234b90
camila-ng <caming2309@gmail.com>
2020-08-17 18:41:33 -0300

    add changes

--- src/data.js
+++ src/data.js
@@ -1,66 +1,66 @@
 const info = [
     {
       id: 1,
-      name: "Mitre",
+      name: "Ivana Sanchez",
       image:
-        "https://www.infobae.com/new-resizer/gu1iX3j020vbS8yh7M6PdaqtadU=/768x1024/filters:format(jpg):quality(85)/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2017/08/24120630/Bartolome-Mitre-1.jpg",
+        "https://images.unsplash.com/photo-1489667897015-bf7a9e45c284?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=806&q=80",
         description:
          "La única verdad es la realidad La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere y defiende un solo interés: el del pueblo La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere"
     },
     {
       id: 2,
-      name: "Sarmiento",
+      name: "Julieta Mullen",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/9/9d/Sarmiento.jpg",
+        "https://images.unsplash.com/photo-1499887142886-791eca5918cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
       description:"La única verdad es la realidad La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere y defiende un solo interés: el del pueblo La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere"
     },
     {
       id: 2,
-      name: "ghghgjtyjktujk",
+      name: "Giuliana Buccheri",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/9/9d/Sarmiento.jpg",
+        "https://images.unsplash.com/photo-1528341866330-07e6d1752ec2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=801&q=80",
       description:"La única verdad es la realidad La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere y defiende un solo interés: el del pueblo La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere"
     },
     {
       id: 2,
-      name: "wetwrtyeyeth",
+      name: "Camila Gonzalez",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/9/9d/Sarmiento.jpg",
+        "https://images.unsplash.com/photo-1530650068191-e80dcd5ee0b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
       description:"La única verdad es la realidad La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere y defiende un solo interés: el del pueblo La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere"
     },
     {
       id: 2,
-      name: "sdfgafdsghadhgdh",
+      name: "Teresa Zappia",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/9/9d/Sarmiento.jpg",
+        "https://images.unsplash.com/photo-1485893086445-ed75865251e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
       description:"La única verdad es la realidad La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere y defiende un solo interés: el del pueblo La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere"
     },
     {
       id: 2,
-      name: "sfgasdgafsgfadsg",
+      name: "Isabel Matazza",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/9/9d/Sarmiento.jpg",
+        "https://images.unsplash.com/photo-1518577915332-c2a19f149a75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=784&q=80",
       description:"La única verdad es la realidad La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere y defiende un solo interés: el del pueblo La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere"
     },
     {
       id: 2,
-      name: "hgdhfg",
+      name: "Romina Fernandez",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/9/9d/Sarmiento.jpg",
+        "https://images.unsplash.com/photo-1552162864-987ac51d1177?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
       description:"La única verdad es la realidad La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere y defiende un solo interés: el del pueblo La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere"
     },
     {
       id: 2,
-      name: "fghdhfhfhfhf",
+      name: "Lucia Perez",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/9/9d/Sarmiento.jpg",
+        "https://images.unsplash.com/photo-1560579183-5a1786c68413?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
       description:"La única verdad es la realidad La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere y defiende un solo interés: el del pueblo La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere"
     },
     {
       id: 2,
-      name: "fgfgffgfggfg",
+      name: "Maia Gomez",
       image:
-        "https://upload.wikimedia.org/wikipedia/commons/9/9d/Sarmiento.jpg",
+        "https://images.unsplash.com/photo-1563701039037-37a8ea2e79be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
       description:"La única verdad es la realidad La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere y defiende un solo interés: el del pueblo La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere"
     },
   ];
