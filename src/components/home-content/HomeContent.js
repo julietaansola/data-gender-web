2606c55a2522ad9be35ad4b90188cc41bb0e573c
maisli <maislipczuk@gmail.com>
2020-10-11 14:57:58 -0300

    home

--- src/components/home-content/HomeContent.js
+++ src/components/home-content/HomeContent.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./home-content.scss";
+import logo from "../../assets/button.png"
+import orga1 from "../../assets/orgas/lasDeSist.png"
+
 const HomeContent = () => {
   return (
     <div className="home-content-container">
@@ -7,25 +10,22 @@ const HomeContent = () => {
        <div className='description-wrapper'> 
         <img
           className="home-content-image"
-          src="https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1002&q=80"
-          alt="logo del observatorio"
-        ></img>
+          src={logo}          alt="logo"
+        />
         <p className="home-content-description">
-          Para conducir a un pueblo la primera condición es que uno haya salido
-          del pueblo, que sienta y piense como el pueblo. Quien se dedica a la
-          conducción debe ser profundamente humanista: el conductor siempre
-          trabaja para los demás, jámas para él La verdadera democracia es
-          aquella donde el gobierno hace lo que el pueblo quiere y defiende un
-          solo interés: el del pueblo El Justicialismo ha dejado de ser la causa
-          de un hombre para ser la causa del pueblo, y por ella sí valdría la
-          pena darlo todo, incluso la vida El Justicialismo ha dejado de ser la
-          causa de un hombre para ser la causa del pueblo, y por ella sí valdría
-          la pena darlo todo, incluso la vida Gobernar es fácil, lo difícil es
-          conducir El Justicialismo necesita apóstoles y para ser apóstol hay
-          
+          Que la recolección y el análisis de datos sea también
+          una puerta a una sociedad más justa.                  
         </p>
       </div>
       
+      <div className="aliadxs">
+        <p className="orgas-title">ORGANIZACIONES ALIADAS</p>
+        <div className="orgas-wrapper">
+          <img className="orga 1" src={orga1} alt=""/>
+          <img className="orga 2" src={orga1} alt=""/>
+          <img className="orga 3" src={orga1} alt=""/>
+        </div>
+      </div>
        </div>
      
     </div>
