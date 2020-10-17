8057efa28755101f75be4d61f49b7e4c2032afbd
maisli <maislipczuk@gmail.com>
2020-10-17 17:53:18 -0300

    data somos

--- src/data.js
+++ src/data.js
@@ -1,67 +1,70 @@
 const info = [
     {
       id: 1,
-      name: "Ivana Sanchez",
+      name: "Ivana Feldfeber",
       image:
-        "https://images.unsplash.com/photo-1489667897015-bf7a9e45c284?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=806&q=80",
-        description:
-         "La única verdad es la realidad La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere y defiende un solo interés: el del pueblo La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere"
+        "https://i.ibb.co/vcpwLHT/Ivana.jpg",
+      description:
+      "Lic. en Educación especializada en alfabetización digital y nuevas tecnologías. Profesora de programación y científica de datos.",
+      contact: "Contacto: linkedin.com/in/ivana-feldfeber / twitter.com/ivanafeld"
     },
     {
       id: 2,
-      name: "Julieta Mullen",
+      name: "Martina Cantaro",
       image:
-        "https://images.unsplash.com/photo-1499887142886-791eca5918cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
-      description:"La única verdad es la realidad La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere y defiende un solo interés: el del pueblo La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere"
+        "https://i.ibb.co/yY0PHtM/Martina.jpg",
+      description:"Licenciada en Psicología. Project Manager de software y data science en LambdaClass, Directora de Challenges en Open Data Science Latinoamérica (ODESLA).",
+      contact: "Contacto: https://www.linkedin.com/in/martinacantaro / https://twitter.com/martinacantaro"
     },
     {
       id: 2,
-      name: "Giuliana Buccheri",
+      name: "Yasmín Belén Quiroga",
       image:
-        "https://images.unsplash.com/photo-1528341866330-07e6d1752ec2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=801&q=80",
-      description:"La única verdad es la realidad La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere y defiende un solo interés: el del pueblo La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere"
+        "https://i.ibb.co/n8z1M66/Processed-with-VSCO-with-f2-preset.jpg",
+      description:"Abogada especialista en género y analista de datos -abodatera-.",
+      contact: "Contacto: https://twitter.com/yasquiroga / https://www.linkedin.com/in/yasminbquiroga/"
     },
     {
       id: 2,
-      name: "Camila Gonzalez",
+      name: "Mailén García",
       image:
-        "https://images.unsplash.com/photo-1530650068191-e80dcd5ee0b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
-      description:"La única verdad es la realidad La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere y defiende un solo interés: el del pueblo La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere"
+        "https://i.ibb.co/6PQrMx5/Mai.jpg",
+      description:"Licenciada en Sociología. Magíster en Derechos Humanos y Democratización en América Latina y el Caribe. Investigadora y docente. Obsesionada por los datos. Ex integrante de equipos de estadística y datos abiertos de la provincia de Buenos Aires y Mar del Plata. ",
+      contact: "Contacto: https://www.linkedin.com/in/mailenagarcia/ / https://twitter.com/maigarcia24"
+      
     },
     {
       id: 2,
-      name: "Teresa Zappia",
+      name: "Sabina Bercovich",
       image:
-        "https://images.unsplash.com/photo-1485893086445-ed75865251e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
-      description:"La única verdad es la realidad La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere y defiende un solo interés: el del pueblo La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere"
+        "https://i.ibb.co/DQ8KfC7/Sabi.jpg",
+      description:"Actuaria especialista en gestión, modelado y lectura de datos sociales. Analista investigadora en las áreas de datos y género en Fundar. Docente universitaria y mentora en ciencia de datos en Acamica.",
+      contact: "Contacto: https://www.linkedin.com/in/sbercovich/ / https://twitter.com/SabiBerco"
     },
     {
       id: 2,
-      name: "Isabel Matazza",
+      name: "Paloma Urtizberea García",
       image:
-        "https://images.unsplash.com/photo-1518577915332-c2a19f149a75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=784&q=80",
-      description:"La única verdad es la realidad La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere y defiende un solo interés: el del pueblo La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere"
+        "https://i.ibb.co/M5YsBBs/Flor-Savoca.jpg",
+      description:"Física. Data Scientist @ Mercado Libre e Instituto del Cálculo. ",
+      contact: "Contacto: https://www.linkedin.com/in/paloma-urtizberea-7a500291 / https://twitter.com/pepaurtiz"
+    },
+    
+    {
+      id: 2,
+      name: "Florencia Savoca Truzzo",
+      image:
+        "https://i.ibb.co/M5YsBBs/Flor-Savoca.jpg",
+      description:"Licenciada en Economía y Magíster en Relaciones Internacionales. Investigadora en The Global Women's Institute.",
+      contact: "Contacto: www.linkedin.com/in/florenciasavocatruzzo / https://twitter.com/FlorSavocaT"
     },
     {
       id: 2,
-      name: "Romina Fernandez",
+      name: "Nadia Fusco",
       image:
-        "https://images.unsplash.com/photo-1552162864-987ac51d1177?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
-      description:"La única verdad es la realidad La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere y defiende un solo interés: el del pueblo La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere"
-    },
-    {
-      id: 2,
-      name: "Lucia Perez",
-      image:
-        "https://images.unsplash.com/photo-1560579183-5a1786c68413?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
-      description:"La única verdad es la realidad La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere y defiende un solo interés: el del pueblo La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere"
-    },
-    {
-      id: 2,
-      name: "Maia Gomez",
-      image:
-        "https://images.unsplash.com/photo-1563701039037-37a8ea2e79be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
-      description:"La única verdad es la realidad La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere y defiende un solo interés: el del pueblo La verdadera democracia es aquella donde el gobierno hace lo que el pueblo quiere"
+        "https://i.ibb.co/X2hkxQS/Nachu-Fusco.jpg",
+      description:"Creativa publicitaria y fotógrafa. Creo campañas y contenidos estratégicos para redes sociales.Me especializo en ONGs y campañas con sentido social. Actualmente estoy en @EidosGlobal, organización de educación. Además tengo mi estudio de fotografía y comunicación comercial. ",
+      contact: "Contacto: https://www.linkedin.com/in/nadiafusco/ / https://www.instagram.com/fusco.studio/"
     },
   ];
 
