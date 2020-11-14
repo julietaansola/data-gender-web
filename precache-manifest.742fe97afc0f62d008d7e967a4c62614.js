ff4bdf173609241c725c504dcff41f9800e5cc94
maisli <maislipczuk@gmail.com>
2020-11-14 18:50:59 -0300

    Updates

---
+++ precache-manifest.742fe97afc0f62d008d7e967a4c62614.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeaa377941ca22eccc6280853a2ad711",
+    "url": "/data-gender-web/index.html"
+  },
+  {
+    "revision": "b3aac312db6a1bf58ad7",
+    "url": "/data-gender-web/static/css/main.1c75f667.chunk.css"
+  },
+  {
+    "revision": "07df9b0537e47a58c858",
+    "url": "/data-gender-web/static/js/2.3af64dd2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/data-gender-web/static/js/2.3af64dd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3aac312db6a1bf58ad7",
+    "url": "/data-gender-web/static/js/main.2959ea2a.chunk.js"
+  },
+  {
+    "revision": "57e203b45ea37f60ce41",
+    "url": "/data-gender-web/static/js/runtime-main.d8d7951c.js"
+  },
+  {
+    "revision": "abb41fcd2de5f9bf96df4d7a387f33c0",
+    "url": "/data-gender-web/static/media/ArcaMajora3-Bold.abb41fcd.ttf"
+  },
+  {
+    "revision": "359490e76c93a9947ed381853b1516f5",
+    "url": "/data-gender-web/static/media/button.359490e7.png"
+  },
+  {
+    "revision": "2005367f61ff3d0f9a3152533ce04c93",
+    "url": "/data-gender-web/static/media/fondo-home.2005367f.png"
+  },
+  {
+    "revision": "c5544d964a172282bec1d2317e278df1",
+    "url": "/data-gender-web/static/media/lasDeSist.c5544d96.png"
+  },
+  {
+    "revision": "2831a866cf26f5555d6ccb2e18b85d71",
+    "url": "/data-gender-web/static/media/metcba.2831a866.png"
+  },
+  {
+    "revision": "0cf0876d34c2d526f223a428fc3da5bc",
+    "url": "/data-gender-web/static/media/mision-1.0cf0876d.png"
+  },
+  {
+    "revision": "a81ae3407ecd7434b670110d75895e0a",
+    "url": "/data-gender-web/static/media/mision-2.a81ae340.png"
+  },
+  {
+    "revision": "84c3f2004193b0b8649cf1a69f592a67",
+    "url": "/data-gender-web/static/media/rladies.84c3f200.png"
+  },
+  {
+    "revision": "6f4e1c1fde860bc1c17e1916cb776c62",
+    "url": "/data-gender-web/static/media/trama.6f4e1c1f.png"
+  },
+  {
+    "revision": "388a09b7fbf4e8f363e447abc82c6f9f",
+    "url": "/data-gender-web/static/media/wbio.388a09b7.png"
+  },
+  {
+    "revision": "a5e73fcdeb0cbd5f93a392d3bdc21836",
+    "url": "/data-gender-web/static/media/wim.a5e73fcd.png"
+  }
+]);
